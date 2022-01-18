06f710c7666046064a186a8006def7105c3dd5d3
Sasha <ashatilovdev@gmail.com>
2022-01-18 19:29:05 +0300

    add new project

--- plugins/ScreenCast/src/ios/CDVCordovaScreencast.swift
+++ plugins/ScreenCast/src/ios/CDVCordovaScreencast.swift
@@ -22,16 +22,6 @@ class BroadcastController : RPBroadcastController {
         previewViewController?.dismiss(animated: true)
     }
     
-    @objc(isBroadcasting:)
-    func isBroadcasting(_ command: CDVInvokedUrlCommand) {
-        var recording = false;
-        if #available(iOS 10.0, *) {
-            recording = BroadcastController.controller.isBroadcasting
-        }
-        let pluginResult = CDVPluginResult(status:CDVCommandStatus_OK, messageAs: recording)
-        self.commandDelegate!.send(pluginResult, callbackId:command.callbackId)
-    }
-    
     weak var broadcastCommand:CDVInvokedUrlCommand?
     @objc(startBroadcast:)
     func startBroadcast(_ command: CDVInvokedUrlCommand) {
@@ -89,24 +79,4 @@ class BroadcastController : RPBroadcastController {
       self.commandDelegate!.evalJs("console.log('hello js')")
     }
 
-    @objc(stopBroadcast:)
-    func stopBroadcast(_ command: CDVInvokedUrlCommand) {
-        if #available(iOS 10.0, *) {
-            let controller = BroadcastController.controller;
-            controller.finishBroadcast { error in
-                if error == nil {
-                    print("Broadcast ended")
-                    var pluginResult:CDVPluginResult
-                    pluginResult = CDVPluginResult(status:CDVCommandStatus_OK)
-                    pluginResult.setKeepCallbackAs(true)
-                    self.commandDelegate!.send(pluginResult, callbackId:command.callbackId)
-                } else {
-                    var pluginResult:CDVPluginResult
-                    pluginResult = CDVPluginResult(status:CDVCommandStatus_ERROR, messageAs: error?.localizedDescription)
-                    pluginResult.setKeepCallbackAs(true)
-                    self.commandDelegate!.send(pluginResult, callbackId:command.callbackId)
-                }
-            }
-        }
-    }
 }
